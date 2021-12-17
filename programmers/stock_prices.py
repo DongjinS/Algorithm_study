829cad1844159f47d9f7d3a7147d93e299762fef
DongjinS <sdjsdj3@gmail.com>
2021-12-17 20:20:20 +0900

    min 삭제 gydbftjd xhdrhk

--- programmers/stock_prices.py
+++ programmers/stock_prices.py
@@ -9,15 +9,13 @@ def solution(prices):
         if not prices:
             answer.append(0)
             break
-        if cur <= min(prices):
-            answer.append(len(prices))
         else:
             for p in prices:
                 i+=1
                 if p<cur:
-                    answer.append(i)
                     break
-
+            answer.append(i)
+    
     return answer
 
 solution([1, 2, 3, 2, 3])
