6cbaf38768cc0325d4cb4cc3000ebe92edc54afa
DongjinS <sdjsdj3@gmail.com>
2022-03-22 00:25:40 +0900

    fix: 위장 and re_hindex

--- programmers/re_hindex.py
+++ programmers/re_hindex.py
@@ -1,17 +1,15 @@
 def solution(citations):
     answer = 0
     n = len(citations)
-    citations.sort()
     h_candidate = []
-    for i in range(n):
+    for i in range(max(citations)):
         h_cnt = 0
-        for j in range(n-1-i,n):
-            if citations[n-1-i] <= citations[j]:
+        for j in range(len(citations)):
+            if i <= citations[j]:
                 h_cnt += 1
-        if h_cnt >= citations[n-1-i]:
-            return citations[n-1-i]
-#             h_candidate.append(citations[n-1-i])
-            
-#     print(h_candidate)
-#     answer = max(h_candidate)
+        if h_cnt >= i:
+            h_candidate.append(i)
+    
+    if h_candidate:
+        answer = h_candidate[-1]
     return answer
--- programmers/위장.py
+++ programmers/위장.py
@@ -1,30 +1,11 @@
 def solution(clothes):
     from collections import defaultdict
-    from itertools import product
-    answer = 0
+    answer = 1
     cloth_dict = defaultdict(list)
     for cloth, category in clothes:
         cloth_dict[category].append(cloth)
-        
-    for category in cloth_dict:
-        answer += len(cloth_dict[category])
-      
-    print(answer)
-    if len(cloth_dict.keys())>1:
-        combi_list = set()
-        container = []
-        for value in cloth_dict.values():
-                   for value2 in cloth_dict.values():
-                     
-            container.append(value)
-            combi_list.add((product(*container)))
-          
-        combi_list = list(map(list, combi_list))
-        for combi in combi_list:
-          print(combi)
-          answer += len(list(map(list,combi)))
     
-    print(answer)
-    return answer
-
-solution([["oneface", "face"], ["twoface", "face"], ["oneshirt", "shirt"], ["onepants", "pants"], ["oneouter", "outer"]])
+    for category in cloth_dict:
+        answer *= len(cloth_dict[category])+1
+    
+    return answer-1
