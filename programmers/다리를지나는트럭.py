93aa577531d1f7df079537c46b89a48c228328ee
DongjinS <sdjsdj3@gmail.com>
2022-03-21 23:59:08 +0900

    다리를 지나는 트럭 fix

--- programmers/다리를지나는트럭.py
+++ programmers/다리를지나는트럭.py
@@ -1,1 +1,23 @@
-다리를지나는트럭.py
+def solution(bridge_length, weight, truck_weights):
+    from collections import deque
+    answer = 1
+    truck_weights.reverse()
+    cur_weight = truck_weights[-1]
+    on_bridge = deque()
+    on_bridge.append([truck_weights.pop(),0])
+    while truck_weights or on_bridge:
+        if on_bridge:
+            for i in range(len(on_bridge)):
+                on_bridge[i][1] += 1
+            if on_bridge[0][1] >= bridge_length:
+                out = on_bridge.popleft()
+                cur_weight -= out[0]
+                
+        if truck_weights and truck_weights[-1] + cur_weight <= weight:
+            new_truck = truck_weights.pop()
+            on_bridge.append([new_truck, 0])
+            cur_weight += new_truck
+        
+        answer += 1
+        
+    return answer
