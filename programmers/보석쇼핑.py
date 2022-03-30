06530282710bf5a4ac4c3906976bca293faabc52
DongjinS <sdjsdj3@gmail.com>
2022-03-30 23:29:36 +0900

    fix 보석쇼핑

--- programmers/보석쇼핑.py
+++ programmers/보석쇼핑.py
@@ -7,34 +7,26 @@ def solution(gems):
     end = 0
     N = len(gems)
     dN = len(set(gems))
-    Full = False
     candidate = []
     
     while True:
-        if not Full and end != N-1:
+        if start == N:
+            break
+        elif len(shopped) == dN:
+            candidate.append([start, end, end-start])
+            shopped[gems[start]] -= 1
+            if shopped[gems[start]] == 0:
+                del shopped[gems[start]]
+            start+=1
+        elif len(shopped) != dN:
+            if end == N:
+                break
             shopped[gems[end]] += 1
             end+=1
-        else:
-            while start != N-1:
-                shopped[gems[start]] -= 1
-                if len(list(filter(lambda n : n > 0 ,list(shopped.values())))) == dN:
-                    start += 1
-                    candidate.append([start, end, end-start])
-                else:
-                    break
-            if end == N-1 or start == N-1:
-                print("break")
-                break
-            Full = False
-        
-        if len(list(filter(lambda n : n > 0 ,list(shopped.values())))) == dN:
-            candidate.append([start, end, start+end])
-            Full = True
             
     if candidate:
         candidate.sort(key=lambda x:x[2])
-        print(candidate)
-        answer = [candidate[0][0], candidate[0][1]]
+        answer = [candidate[0][0]+1, candidate[0][1]]
     else:
         answer = [1,N]
     return answer
