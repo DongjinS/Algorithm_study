8491118862228672a5a42c026c179e41abdc4055
DongjinS <sdjsdj3@gmail.com>
2022-04-18 00:23:31 +0900

    Solved: programmers 17677

--- programmers/뉴스_클러스터링.py
+++ programmers/뉴스_클러스터링.py
@@ -1,32 +1,36 @@
 def solution(str1, str2):
     answer = 0
-    first = set()
-    second = set()
-
-    str1 = list(filter(lambda x: x.isalpha(), str1))
-    str2 = list(filter(lambda x: x.isalpha(), str2))
+    first = list()
+    second = list()
 
     for i in range(len(str1)-1):
         if str1[i].isalpha() and str1[i+1].isalpha():
-            first.add(str1[i].lower()+str1[i+1].lower())
+            first.append(str1[i].lower()+str1[i+1].lower())
     
     for i in range(len(str2)-1):
         if str2[i].isalpha() and str2[i+1].isalpha():
-            second.add(str2[i].lower()+str2[i+1].lower())
+            second.append(str2[i].lower()+str2[i+1].lower())
     
-    # print(first, second)
+    inter = []
+    uni = list(map(lambda x:x, second))
 
-    inter = first.intersection(second)
-    union = first.union(second)
+    for elem in first:
+        if elem in second:
+            inter.append(elem)
+            second.remove(elem)
+        else:
+            uni.append(elem)
 
-    print(inter, union)
-
-    answer = int((len(inter)/len(union)) * 65536)
-    print(answer)
-
+    j_sim = 1
+    if uni:
+        j_sim = float(len(inter))/len(uni)
+    
+    answer = int(j_sim * 65536)
+    
     return answer
 
 solution("FRANCE", "french")
 solution("handshake", "shake hands")
 solution("aa1+aa2",	"AAAA12")
 solution("E=M*C^2",	"e=m*c^2")
+solution("A+C","DEF")
