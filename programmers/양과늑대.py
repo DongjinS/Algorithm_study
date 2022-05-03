156f7274b22a61f794da7b942489a2233a003c22
DongjinS <sdjsdj3@gmail.com>
2022-05-04 05:47:55 +0900

    Solve: programmers 92343

--- programmers/양과늑대.py
+++ programmers/양과늑대.py
@@ -1,65 +1,37 @@
-# def solution(info, edges):
-#     from collections import defaultdict
-#     from collections import deque
-#     answer = 0
+def dfs(nsheep, nwolf, cur, adjacent, info, graph):
+    if info[cur]:
+        nwolf+=1
+    else:
+        nsheep+=1
+
+    if nwolf>=nsheep:
+        return 0
+
+    sheep_max = nsheep
+
+    for adj in adjacent:
+        for n in graph[adj]:
+            if n not in adjacent:
+                adjacent.add(n)
+                sheep_max = max(sheep_max,dfs(nsheep, nwolf, n, adjacent, info, graph))
+                adjacent.remove(n)
     
-#     map = defaultdict(list)
-#     for s,e in edges:
-#         map[s].append([e,info[e]])
-#         map[e].append([s,info[s]])
-
-#     for node in map:
-#         map[node].sort(key = lambda x:x[1])
-
-#     print(map)
-#     dq = deque([0])
-#     nsheep = 0
-#     nwolf = 0
-#     chk = [True for x in range(len(info))]
-#     while dq:
-        
-#         break
-                
-#     print(nsheep)
-#     return nsheep
+    return sheep_max
 
 def solution(info, edges):
-    def nextNodes(v):
-        temp = list()
-        for e in edges:
-            # i는 부모노드, j는 자식노드
-            i, j = e
-            # 부모노드 번호 비교
-            if v == i:
-                temp.append(j)
-        return temp
+    from collections import defaultdict
+    answer = 0
+    
+    graph = defaultdict(list)
+    for s,e in edges:
+        graph[s].append(e)
+        graph[e].append(s)
 
-    def dfs(sheep, wolf, current, path):
-        # 지금 노드 확인, 양 늑대 판별
-        if info[current]:
-            wolf += 1
-        else:
-            sheep += 1
-
-        # 늑대가 다 잡아먹음, 무시
-        if sheep <= wolf:
-            return 0
-
-        # 아니라면 임시 변수에 값 갱신
-        maxSheep = sheep
-
-        # 서칭 시작
-        for p in path:
-            for n in nextNodes(p):
-                if n not in path:
-                    path.append(n)
-                    # 최대 양 판별
-                    print(sheep, wolf, n, path)
-                    maxSheep = max(maxSheep, dfs(sheep, wolf, n, path))
-                    path.pop()
-        return maxSheep
-
-    answer = dfs(0, 0, 0, [0])
+    print(graph)
+    adjacent = set()
+    adjacent.add(0)
+    answer = dfs(0, 0, 0, adjacent, info, graph)
+    
     print(answer)
     return answer
 
