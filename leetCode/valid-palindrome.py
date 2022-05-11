5207348f94c20b5f78476171342bdc6dd0e3f848
DongjinS <sdjsdj3@gmail.com>
2022-05-12 00:44:43 +0900

    Feat: add new function with using regular expression

--- leetCode/valid-palindrome.py
+++ leetCode/valid-palindrome.py
@@ -8,4 +8,13 @@ class Solution:
                 lower_s.append(c)
                 
         if lower_s==list(reversed(lower_s)):return True
-        else:return False        
+        else:return False
+
+    #정규식 이용
+    def isPalindrome2(self, s: str) -> bool:
+        lower_s = s.lower()
+        lower_s = re.sub('[^a-z0-9]','',lower_s)
+        return lower_s==lower_s[::-1]
+
+
+Solution.isPalindrome2(Solution,"A man, a plan, a canal: Panama")
